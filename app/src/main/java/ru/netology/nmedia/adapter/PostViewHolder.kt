94f03d132ef694caf275c34544465fa7dedc81bf
teodorro <edem.teo@gmail.com>
2021-03-28 18:35:32 +0300

    split

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
@@ -0,0 +1,55 @@
+package ru.netology.nmedia.adapter
+
+import android.widget.PopupMenu
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.BuildConfig
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.view.loadCircleCrop
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onInteractionListener: OnInteractionListener,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            avatar.loadCircleCrop("${BuildConfig.BASE_URL}/avatars/${post.authorAvatar}")
+            like.isChecked = post.likedByMe
+            like.text = "${post.likes}"
+
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.options_post)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove -> {
+                                onInteractionListener.onRemove(post)
+                                true
+                            }
+                            R.id.edit -> {
+                                onInteractionListener.onEdit(post)
+                                true
+                            }
+
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+
+            like.setOnClickListener {
+                onInteractionListener.onLike(post)
+                like.isChecked = !like.isChecked
+            }
+
+            share.setOnClickListener {
+                onInteractionListener.onShare(post)
+            }
+        }
+    }
+}
